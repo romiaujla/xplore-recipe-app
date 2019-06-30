25c670bdf0f5eee5abdf5567776bd31003a845ed
Raman Aujla <ramanaujla@Ramneets-MacBook-Air.local>
2019-06-30 11:54:19 -0400

    Added wireframe sketches

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function watchForm(){
+    
+    // Submit Button Event Handler
+    $('.search-form').on('submit', function(e){
+        e.preventDefault();
+
+        // get value entered by the user
+        let userSearchQuery = $('.search-textbox').val();
+
+        
+        
+    });
+
+}
+
+$(watchForm);
